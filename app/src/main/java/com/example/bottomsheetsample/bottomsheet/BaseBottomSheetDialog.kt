1d4c4eb74bde0a4907b9da79d5fa3b6f13c06f9d
Kang Taehee <dev.thk28@gmail.com>
2023-11-29 14:02:59 +0900

    FEAT: 바텀시트 다이얼로그가 완전히 펼쳐지도록 변경

--- app/src/main/java/com/example/bottomsheetsample/bottomsheet/BaseBottomSheetDialog.kt
+++ app/src/main/java/com/example/bottomsheetsample/bottomsheet/BaseBottomSheetDialog.kt
@@ -1,10 +1,14 @@
 package com.example.bottomsheetsample.bottomsheet
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.bottomsheetsample.databinding.DialogBottomSheetBaseBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 abstract class BaseBottomSheetDialog : BottomSheetDialogFragment() {
@@ -29,6 +33,25 @@ abstract class BaseBottomSheetDialog : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // 바텀시트 내용이 커도 한번에 펼쳐지도록, 중간에 걸리지 않도록 설정
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        dialog.setOnShowListener { dialogInterface ->
+            val _dialog = dialogInterface as BottomSheetDialog
+
+            _dialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)?.also {
+                BottomSheetBehavior.from(it).apply {
+                    state = BottomSheetBehavior.STATE_EXPANDED
+                    skipCollapsed = true
+                }
+
+            }
+        }
+
+        return dialog
     }
 
     override fun onDestroyView() {
